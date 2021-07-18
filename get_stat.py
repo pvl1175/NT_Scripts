d8ccde64c6a2cb20060ff37681544e2f761303a0
Vitaly Popov <vitaly_popov@msn.com>
2021-07-18 15:13:33 +0300

    m

--- get_stat.py
+++ get_stat.py
@@ -1,5 +1,6 @@
 import sqlite3 as lite
 from io import StringIO, BytesIO
+from fpdf import FPDF
 
 from docx import Document
 import pdfkit
@@ -128,110 +129,110 @@ class Stat:
 
         return files
 
+    def reports_pdf(self, num1, num2):
+        pdf = FPDF(format='a4', unit='cm')
+        pdf.add_page()
+        pdf.set_font('Times', '', 10.0)
+        epw = pdf.w - 2 * pdf.l_margin
+        col_width = epw / 4
+        th = pdf.font_size
+
+        files = []
+        for table in self.__tables():
+            data = self.__get_data(num1, num2, table[0])
+            pdf.cell(col_width, th, str('Number'), border=1)
+            pdf.cell(col_width, th, str('Out'), border=1)
+            pdf.cell(col_width, th, str('In'), border=1)
+            pdf.ln(th)
+
+            for data_row in data:
+                pdf.cell(col_width, th, str(data_row[0]), border=1)
+                pdf.cell(col_width, th, str(data_row[1]), border=1)
+                pdf.cell(col_width, th, str(data_row[2]), border=1)
+                pdf.ln(th)
+
+            files.append(['{0}.pdf'.format(table[0]), pdf.output(dest='S').encode('latin1')])
+
+        return files
 
     def reports(self, num1, num2):
-        path = 'd:/temp'
-
-        header = '''
-<!DOCTYPE html>
-<html>
-<head>
-<style>
-    table, th, td { border: 1px solid black; }
-</style>
-</head>
-<body>
-        '''
-        table_header = '''
-    <br>
-    <table style="width:100%">
-        <tr>
-            <th>Номер</th>
-            <th>Количество исходящих</th> 
-            <th>Количество входящих</th>
-        </tr>  
-        '''
-        table_footer = '\n\t</table>'
-        footer = '''
-</body>
-</html>
-        '''
-        body = ''
-
         resources = []
-        for table in self.__tables():
-            body = body + table_header
-            data = self.__get_data(num1, num2, table[0])
-            for row in data:
-                body = body + '<tr>'
-                body = body + '\n' + '\t\t<td>' + row[0] + '</td>'
-                body = body + '\n' + '\t\t<td>' + str(row[1]) + '</td>'
-                body = body + '\n' + '\t\t<td>' + str(row[2]) + '</td>'
-                body = body + '\n' + '\t</tr>\n\t'
-            body = body + table_footer
 
-            # build html file
-            f = open('{0}/{1}.html'.format(path, table[0]), 'w')
-            f.write(header + body + footer)
-            f.close()
+        files_html = self.reports_html(num1, num2)
+        files_docx = self.reports_docx(num1, num2)
+        files_pdf = self.reports_pdf(num1, num2)
 
-            # convert to docx
-            new_parser = HtmlToDocx()
-            new_parser.parse_html_file('{0}/{1}.html'.format(path, table[0]), '{0}/{1}'.format(path, table[0]))
-
-            # convert to pdf
-            pdfkit.from_file('{0}/{1}.html'.format(path, table[0]), '{0}/{1}.pdf'.format(path, table[0]))
-
-            files = ['{0}/{1}.html'.format(path, table[0]), '{0}/{1}.docx'.format(path, table[0]),
-                     '{0}/{1}.pdf'.format(path, table[0])]
+        index = 0
+        for _ in self.__tables():
+            files = []
+            files.append([files_html[index][0], files_html[index][1]])
+            files.append([files_docx[index][0], files_docx[index][1]])
+            files.append([files_pdf[index][0], files_pdf[index][1]])
 
             resources.append(files)
+            index = index + 1
 
         return resources
 
-# class StatHeader(metaclass=Header):
-#     display_name = 'Stat'
-#     resource_html = Field('HTML', ValueType.String, binary_type=BinaryType.Resource)
-#     resource_docx = Field('DOCX', ValueType.String, binary_type=BinaryType.Resource)
-#     resource_pdf = Field('PDF', ValueType.String, binary_type=BinaryType.Resource)
-#
-# class SqliteTask(Task):
-#
-#     def get_id(self):
-#         return 'b795762d-6a93-4bc6-8a42-6440d400107f'
-#
-#     def get_category(self):
-#         return 'StatTask'
-#
-#     def get_display_name(self):
-#         return 'Stat Task'
-#
-#     def get_headers(self):
-#         return HeaderCollection(StatHeader)
-#
-#     def get_enter_params(self):
-#         return EnterParamCollection(
-#             EnterParamField('path', 'Path', ValueType.String, is_array=False, required=True, file_path=True,
-#                             default_value='', category='Required', description='Path'))
-#
-#     @staticmethod
-#     def __fill_result(result_writer, resources):
-#         for resource in resources:
-#             line = StatHeader.create_empty()
-#             line[StatHeader.resource_html] = Resource(resource[0], open(resource[0], 'rb').read())
-#             line[StatHeader.resource_docx] = Resource(resource[1], open(resource[1], 'rb').read())
-#             line[StatHeader.resource_pdf] = Resource(resource[2], open(resource[2], 'rb').read())
-#
-#             result_writer.write_line(line, header_class=StatHeader)
-#
-#     def execute(self, enter_params, result_writer, log_writer, temp_directory):
-#         stat = Stat(enter_params.path)
-#         self.__fill_result(result_writer, stat.reports('num1', 'num2'))
+class StatHeader(metaclass=Header):
+    display_name = 'Stat'
+    resource_html = Field('HTML', ValueType.String, binary_type=BinaryType.Resource)
+    resource_docx = Field('DOCX', ValueType.String, binary_type=BinaryType.Resource)
+    resource_pdf = Field('PDF', ValueType.String, binary_type=BinaryType.Resource)
 
-# stat = Stat('D:\\dev\\nt\\lampyre\\master\\AS\\AS.UV\\bin\\Debug\\net472\\!sqlite_project\\d3vckf5c.sbe\\ca6b84c20b7644a3b9fbf6170876f3bf.sqlite')
-# files = stat.reports_docx('num1', 'num2')
-# print(files)
+class SqliteTask(Task):
+
+    def get_id(self):
+        return 'b795762d-6a93-4bc6-8a42-6440d400107f'
+
+    def get_category(self):
+        return 'StatTask'
+
+    def get_display_name(self):
+        return 'Stat Task'
+
+    def get_headers(self):
+        return HeaderCollection(StatHeader)
+
+    def get_enter_params(self):
+        return EnterParamCollection(
+            EnterParamField('path', 'Path', ValueType.String, is_array=False, required=True, file_path=True,
+                            default_value='', category='Required', description='Path'))
+
+    @staticmethod
+    def __fill_result(result_writer, resources):
+        for resource in resources:
+            line = StatHeader.create_empty()
+            line[StatHeader.resource_html] = Resource(resource[0][0], resource[0][1].encode())
+            line[StatHeader.resource_docx] = Resource(resource[1][0], resource[1][1])
+            line[StatHeader.resource_pdf] = Resource(resource[2][0], resource[2][1])
+            # line[StatHeader.resource_html] = Resource(resource[0][0], resource[0][1])
+            # line[StatHeader.resource_docx] = Resource(resource[1][0], resource[1][1])
+            # line[StatHeader.resource_pdf] = Resource(resource[2][0], resource[2][1])
+
+            result_writer.write_line(line, header_class=StatHeader)
+
+    def execute(self, enter_params, result_writer, log_writer, temp_directory):
+        stat = Stat(enter_params.path)
+        self.__fill_result(result_writer, stat.reports('num1', 'num2'))
+
+# stat = Stat('D:\\dev\\nt\\lampyre\\master\\AS\\AS.UV\\bin\\Debug\\net472\\!sqlite_project\\3b5cbesa.rk3\\ca6b84c20b7644a3b9fbf6170876f3bf.sqlite')
+# resources = stat.reports('num1', 'num2')
 #
-# file = open("d:/temp/test.docx", "wb")
-# file.write(files[0][1])
+# for resource in resources:
+#     print(resource[0][0])
+#     print(resource[1][0])
+#     print(resource[2][0])
+
+#
+# file = open("d:/temp/" + resources[0][0][0], "w")
+# file.write(resources[0][0][1])
+# file.close()
+#
+# file = open("d:/temp/" + resources[0][1][0], "wb")
+# file.write(resources[0][1][1])
+# file.close()
+#
+# file = open("d:/temp/" + resources[0][2][0], "wb")
+# file.write(resources[0][2][1])
 # file.close()
